a7402990ce8fba14a92f3979e072910c1dc66550
yamasy1549 <sanae@yamasy.info>
2016-07-03 01:21:50 +0900

    Set to add tags

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -69,6 +69,6 @@ class DesignsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def design_params
-      params.require(:design).permit(:title, :image)
+      params.require(:design).permit(:title, :image, :tag_list)
     end
 end
--- app/models/design.rb
+++ app/models/design.rb
@@ -1,4 +1,5 @@
 class Design < ActiveRecord::Base
+  acts_as_taggable
   attr_accessor :title, :image
 
   mount_uploader :image, ImageUploader
