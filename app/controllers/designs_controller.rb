ddb3beaa1a146164a0dd8b0290fe1186509dfd78
yamasy1549 <sanae@yamasy.info>
2016-07-03 22:48:32 +0900

    Add custom_tags

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -60,7 +60,7 @@ class DesignsController < ApplicationController
     end
 
     def design_params
-      params.require(:design).permit(:title, :image, :color_tag_list)
+      params.require(:design).permit(:title, :image, :color_tag_list, :custom_tag_list)
     end
 
     def get_color_info
--- app/models/design.rb
+++ app/models/design.rb
@@ -1,5 +1,5 @@
 class Design < ActiveRecord::Base
-  acts_as_taggable_on :color_tags
+  acts_as_taggable_on :color_tags, :custom_tags
   attr_accessor :image
 
   mount_uploader :image, ImageUploader
